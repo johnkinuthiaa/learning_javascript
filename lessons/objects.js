08f3d5e0bee30d7b844f1c786360ee794b7590b5
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-14 23:00:50 +0300

    objects

---
+++ lessons/objects.js
@@ -0,0 +1,16 @@
+// store data in key value pairs--left side is variable name(key ) while the right side is the value of the key
+
+let player ={
+    name:"john",
+    age :19
+}
+console.log(player.age)
+
+// you can also store an array in the object
+let houseListing ={
+    name:"king arcade",
+    location:"rongai",
+    pics:["https:ertgfertg.png","https:ertgfertg.png"]
+}
+
+console.log(houseListing.pics)
