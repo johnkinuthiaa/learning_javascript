beb168ca802c96ec6b264ec0ced08faa6a253b84
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-15 18:12:03 +0300

    input

--- scrimba/randomPasswordGenerator/index.js
+++ scrimba/randomPasswordGenerator/index.js
@@ -1,31 +1,55 @@
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
-let isAlive =false
+const letters =["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+const numbers =[ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+const symbols =["~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+"/"];
 
 let password1 = document.getElementById("password-el")
 let password2=document.getElementById("password-ell")
-function generateRandomNumber(){
-    let randomNumber =Math.floor(Math.random()*characters.length)
+
+
+
+function generateRandomNumber(input){
+    let randomNumber =Math.floor(Math.random()*input.length)
     return randomNumber
 }
-function generatePass(){
-    if(isAlive ===false){
-        for(let i=0;i<16;i++){
-            password1.innerHTML +=characters[generateRandomNumber()]
-            password2.innerHTML +=characters[generateRandomNumber()]
-        }
-        isAlive =true
 
+function generatePass(){
+    password1.innerHTML =""
+    password2.innerHTML =""
+    generateNewPassword()
     }
-    else{
-        password1.innerHTML =" "
-        password2.innerHTML =" "
-        generateNewPassword()
-    }
-}
 function generateNewPassword(){
-    for(let i=0;i<16;i++){
-        password1.innerHTML +=characters[generateRandomNumber()]
-        password2.innerHTML +=characters[generateRandomNumber()]
+    let lengthOfPassword =document.getElementById("pass-length")
+    
+    length =lengthOfPassword.value
+    if(length ==="" ||length<3){
+        length =15
+    }
+    let passwordFilter =document.getElementById("select")
+    let passwordFormat =passwordFilter.value
+    if(passwordFormat ==="1"){
+        for(let i=0;i<length;i++){
+            password1.innerHTML +=characters[generateRandomNumber(characters)]
+            password2.innerHTML +=characters[generateRandomNumber(characters)]
+        }
+    }else if(passwordFormat==="2"){
+        for(let i=0;i<length;i++){
+            password1.innerHTML +=letters[generateRandomNumber(letters)]
+            password2.innerHTML +=letters[generateRandomNumber(letters)]
+        }
+    }else if(passwordFormat==="3"){
+        let lettersAndNumbers =letters.concat(numbers)
+        for(let i=0;i<length;i++){
+            password1.innerHTML +=lettersAndNumbers[generateRandomNumber(lettersAndNumbers)]
+            password2.innerHTML +=lettersAndNumbers[generateRandomNumber(lettersAndNumbers)]
+        }
+    }else if(passwordFormat==="4"){
+        let lettersAndSymbols =letters.concat(symbols)
+        for(let i=0;i<length;i++){
+            password1.innerHTML +=lettersAndSymbols[generateRandomNumber(lettersAndSymbols)]
+            password2.innerHTML +=lettersAndSymbols[generateRandomNumber(lettersAndSymbols)]
+        }
     }
 }
