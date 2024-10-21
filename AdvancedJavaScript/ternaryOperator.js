9407bd1a930daa6a112b2e21265795a034fb270a
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-22 01:09:50 +0300

    today

---
+++ AdvancedJavaScript/ternaryOperator.js
@@ -0,0 +1,16 @@
+const exerciseTimeMins = 20
+
+// let message = ''
+
+// if (exerciseTimeMins < 30) {'You need to try harder
+//     message = !'
+// }
+// else {
+//     message = 'Doing good!'
+// }
+
+// console.log(message)
+
+const message = (exerciseTimeMins<30) ? "You need to try harder!" : "Doing good!"
+
+console.log(message)
