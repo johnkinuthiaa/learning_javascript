24f79f7f1732302cd02ac8894f1773008eb189b4
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-10-27 20:14:31 +0300

    for of loop

---
+++ AdvancedJavaScript/forOf.js
@@ -0,0 +1,31 @@
+const characters = [
+    {
+        title: 'Ninja',
+        emoji: '🥷',
+        powers: ['agility', 'stealth', 'aggression'],
+    },
+    {
+        title: 'Sorcerer',
+        emoji: '🧙',
+        powers: ['magic', 'invisibility', 'necromancy'],
+    },
+    {
+        title: 'Ogre',
+        emoji: '👹',
+        powers: ['power', 'stamina', 'shapeshifting'],
+    },
+    {
+        title: 'Unicorn',
+        emoji: '🦄',
+        powers: [ 'flight', 'power', 'purity'],
+    }
+]
+
+//looping using for of
+
+for(let character of characters ){
+    // console.log(character)
+    const { title,emoji,powers} =character;
+    console.log(`title: ${title} ,Emoji: ${emoji},powers: ${powers}`)
+
+}
