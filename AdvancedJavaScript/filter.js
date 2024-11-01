8304554db8b95b00c9b962d8ac6b356ee16e882a
johnkinuthiaa <kinuthiajohn122@gmail.com>
2024-11-01 12:51:56 +0300

    array methods

--- AdvancedJavaScript/filter.js
+++ AdvancedJavaScript/filter.js
@@ -1,4 +1,6 @@
 // mostly used for searches
+let newDiv =''
+const container =document.getElementById("container")
 const series = [
     {
         name: 'The Wire',
@@ -31,6 +33,69 @@ const series = [
         genres: ['comedy', 'nerd', 'romance']
     },
 ]
+
+
+// series.forEach(function(show){
+//     console.log(show)
+// })
+// console.log(thr)
+
+function getAllShows(){
+    for(let show of series){
+        const{ name,location,lengthInHours,genres} =show
+        newDiv+=`
+                <div class="card">
+                    <p>${name}</p>
+                    <p>${location}</p>
+                    <p>${lengthInHours}</p>
+                    <p>${genres}</p>
+                </div>>`
+
+    }
+    container.innerHTML =newDiv
+
+
+}
+// getAllShows()
+// function all(){
+//     const newSeries =series.map(function(show){
+//         const{ name,location,lengthInHours,genres} =show
+//         newDiv+=`
+//                 <div class="card">
+//                     <p>${name}</p>
+//                     <p>${location}</p>
+//                     <p>${lengthInHours}</p>
+//                     <p>${genres}</p>
+//                 </div>>`
+//         container.innerHTML =newDiv
+//
+//     })
+//
+// }
+// all()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 // get shows in atl
 // const loc =series.filter(function(show){
 //     return show.location ==="Atlanta"
@@ -38,13 +103,13 @@ const series = [
 // })
 // console.log(loc)
 // get shows which are thrillers
-const thrillers =series.filter(function(show){
-    return show.genres.includes("thriller")
-})
-
-console.log(thrillers)
-
-const nerds = series.filter(function(show){
-    return show.genres.includes("nerd")
-})
-console.log(nerds)
+// const thrillers =series.filter(function(show){
+//     return show.genres.includes("thriller")
+// })
+//
+// console.log(thrillers)
+//
+// const nerds = series.filter(function(show){
+//     return show.genres.includes("nerd")
+// })
+// console.log(nerds)
